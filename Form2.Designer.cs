fa62e48290bbb5f77279c7cad10a80cb9c3a6f49
teyfik80 <115842019+teyfik80@users.noreply.github.com>
2022-12-15 03:09:55 +0300

    Teyfik Kamber Şamlı ' nın  ödevi

---
+++ Form2.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace OgrenciNotKayitSistemi
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(647, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 97);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Öğrenci Ekle";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, -1);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(601, 477);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(647, 271);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 93);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Yenile";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(826, 476);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private DataGridView dataGridView1;
+        private Button button2;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace OgrenciNotKayitSistemi
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            veriler();
+        }
+
+
+        // veriler kaydedilen ögrencileri gostermek için kullanılır.
+        
+
+        public void veriler()
+        {
+            dataGridView1.Columns.Clear(); 
+
+            dataGridView1.ColumnCount = 3;
+            dataGridView1.Columns[0].Name = "Id";
+            dataGridView1.Columns[1].Name = "Numara";
+            dataGridView1.Columns[2].Name = "Adi Soyadi";
+            foreach (Ogrenci ogrenci in Ogrenciler.ogrenciler)
+            {
+                dataGridView1.Rows.Add(ogrenci.getId(), ogrenci.getNumara(), ogrenci.getAdiSoyadi());
+            }
+
+            DataGridViewButtonColumn dgvButton = new DataGridViewButtonColumn();
+            dgvButton.FlatStyle = FlatStyle.System;
+
+            dgvButton.HeaderText = "Düzenle";
+            dgvButton.Name = "Button";
+            dgvButton.UseColumnTextForButtonValue = true;
+            dgvButton.Text = "Not Gir";
+
+            dataGridView1.Columns.Add(dgvButton);
+
+            DataGridViewButtonColumn dgvButton1 = new DataGridViewButtonColumn();
+            dgvButton1.FlatStyle = FlatStyle.System;
+
+            dgvButton1.HeaderText = "Sil";
+            dgvButton1.Name = "Button";
+            dgvButton1.UseColumnTextForButtonValue = true;
+            dgvButton1.Text = "Sil";
+
+            dataGridView1.Columns.Add(dgvButton1);
+
+        }
+
+        //  ogrenciler listesini güncellemek için kullanılır(yenile)
+        private void button2_Click(object sender, EventArgs e)
+        {
+            veriler();
+
+        }
+
+        //listelenen ögrencileri silmek veya ogrencilere not girişi yapmak için kullanılan butonların metodu.
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            if (e.ColumnIndex == 3 && Ogrenciler.ogrenciler.Count != e.RowIndex)
+            {
+
+                Form3 form3 = new Form3(Int32.Parse(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString()));
+                form3.Show();
+
+            }
+            if (e.ColumnIndex == 4 && Ogrenciler.ogrenciler.Count != e.RowIndex)
+            {
+
+                foreach(Ogrenci ogrenci in Ogrenciler.ogrenciler)
+                {
+                    if(ogrenci.getId() == Int32.Parse(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString()))
+                    {
+                        Ogrenciler.ogrenciler.Remove(ogrenci);
+                        break;
+                    }
+                }
+                veriler();
+            }
+        }
+        //  yeni ogrenci kaydı penceresini açma
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.Show();
+        }
+
+        
+    }
+}
